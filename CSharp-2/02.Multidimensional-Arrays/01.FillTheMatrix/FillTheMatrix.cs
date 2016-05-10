da8ccfc9433f56783ed21aecc03882282ab3eae3
olebg <olebg01@gmail.com>
2016-05-10 18:34:52 +0300

    some homeworks

--- CSharp-2/02.Multidimensional-Arrays/01.FillTheMatrix/FillTheMatrix.cs
+++ CSharp-2/02.Multidimensional-Arrays/01.FillTheMatrix/FillTheMatrix.cs
@@ -7,20 +7,78 @@ class FillTheMatrix
         int n = int.Parse(Console.ReadLine());
         char symbol = char.Parse(Console.ReadLine());
         int[,] matrix = new int[n, n];
-        int start = 1;
 
         if (symbol == 'a')
         {
-            for (int col = 0; col < n; col++)
+            CaseA(matrix, n);
+        }
+        else if (symbol == 'b')
+        {
+            CaseB(matrix, n);
+        }
+
+        for (int col = 0; col < n; col++)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write(matrix[col, row]);
+                if (row != n - 1)
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void CaseA(int[,] matrix, int n)
+    {
+        int count = 1;
+
+        for (int col = 0; col < n; col++)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row, col] = count;
+                count++;
+            }
+        }
+    }
+
+
+    static void CaseB(int[,] matrix, int n)
+    {
+        int count = 1;
+
+        for (int col = 0; col < n; col++)
+        {
+            if (col % 2 == 0)
             {
                 for (int row = 0; row < n; row++)
                 {
-                    matrix[row, col] = start;
-                    start++;
-                    Console.Write("{0,3}", matrix[row, col]);
+                    matrix[row, col] = count;
+                    count++;
                 }
-                Console.WriteLine();
             }
+            else
+            {
+                for (int row = n - 1; row >= 0; row--)
+                {
+                    matrix[row, col] = count;
+                    count++;
+                }
+            }
+
+        }
+    }
+
+    static void CaseC(int[,] matrix, int n)
+    {
+        int count = 1;
+
+        for (int col = 0; col < n; col++)
+        {
+
         }
     }
 }

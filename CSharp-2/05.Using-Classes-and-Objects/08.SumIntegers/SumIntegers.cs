6c6d0caef26be472831d3941412ed0e4926bc81e
olebg <olebg01@gmail.com>
2016-06-09 18:30:12 +0300

    homeworks

---
+++ CSharp-2/05.Using-Classes-and-Objects/08.SumIntegers/SumIntegers.cs
@@ -0,0 +1,35 @@
+﻿namespace SumIntegers
+{
+    using System;
+
+    class SumIntegers
+    {
+        static void Main()
+        {
+            long[] input = ReadInput();
+
+            long result = SumOfNumbers(input);
+
+            Console.WriteLine(result);
+        }
+
+        static long[] ReadInput()
+        {
+            string input = Console.ReadLine();
+            string[] inputSplited = input.Split(' ');
+            long[] numbers = Array.ConvertAll(inputSplited, long.Parse);
+
+            return numbers;
+        }
+
+        static long SumOfNumbers(long[] numbers)
+        {
+            long sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
